ab305a0f91de57eec3be79e398a47c416920bad2
Jeremy Tice <jeremy@requincreative.com>
2023-06-22 19:47:32 -0500

    Init and Highlight Active Links

--- js/script.js
+++ js/script.js
@@ -1,2 +1,46 @@
+const global = {
+    currentPage: window.location.pathname
+}
+
 const searchForm = document.querySelector('.search-form')
 const searchInput = document.querySelector('#search-term')
+const nav = document.querySelector('nav')
+
+// Highlight active link
+const highlightActiveLink = () => {
+    nav.querySelectorAll('a').forEach((link) => {
+        if (link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active')
+        }
+    })
+}
+
+
+
+// Init App
+const init = () => {
+    switch (global.currentPage) {
+        case '/':
+        case '/index.html':
+            console.log('Home');
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+
+    }
+
+    highlightActiveLink()
+}
+
+
+document.addEventListener('DOMContentLoaded', init)
